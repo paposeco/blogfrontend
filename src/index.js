123a2c7d729d7f9b35b7fb0ca6434cfdd3e73a5f
paposeco <13892562+paposeco@users.noreply.github.com>
2023-06-21 11:29:12 +0100

    hashrouter instead of browserrouter for gh-pages

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./scss/custom.scss";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 // Bootstrap CSS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
